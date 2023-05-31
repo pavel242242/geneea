5cbcdf113b465d39963d03be1765bcb03479c5e9
pavel242242 <pavel@chocholous.net>
2023-05-31 14:49:09 +0200

    fixes

---
+++ chat.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from pathlib import Path
+
+openai_token = st.sidebar.text_input("openai token")
+
+
+def loadExistingIndex(persist_directory: Path, embeddings):
+    vectordb = Chroma(persist_directory=str(persist_directory),
+                      embedding_function=embeddings)
+    return vectordb
+
+
+PERSIST_PATH = "out/files"
+OPENAI_KEY = openai_token
+
+from IPython.display import HTML, display
+
+
+def findAnswer(query: str):
+    result = qa({"question": query, "chat_history": []})
+    st.write(result["answer"])
+    st.write("--")
+    st.write('sources:',
+             [d.metadata['source'] for d in result["source_documents"]])
+    st.write("--")
+
+
+question = st.sidebar.text_input("question")
+
+if st.sidebar.button("Click me") and question and openai_token:
+    if OPENAI_KEY:
+        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_KEY)
+        chatOpenAI = ChatOpenAI(openai_api_key=OPENAI_KEY,
+                                temperature=0,
+                                model_name="gpt-3.5-turbo")
+        vectordb = loadExistingIndex(Path(PERSIST_PATH), embeddings)
+
+        qa = ConversationalRetrievalChain.from_llm(
+            chatOpenAI, vectordb.as_retriever(), return_source_documents=True)
+        findAnswer(question)
