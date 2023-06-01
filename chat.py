164f1e588b226403ef49b1e4200e108099f3a30e
pavel242242 <pavel@chocholous.net>
2023-06-01 09:12:13 +0000

    if

--- chat.py
+++ chat.py
@@ -33,7 +33,7 @@ question = st.sidebar.text_input("question")
 if os.getenv('openai_apitoken'):
         st.sidebar.text("using openai token from env: "+ os.getenv('openai_apitoken')[0:6] + "...")
         openai_token = os.getenv('openai_apitoken')
-    else:
+else:
         openai_token = st.sidebar.text_input("openai token")
 
 if st.sidebar.button("Click me") and question and openai_token:
