600f6aec1da5f79ee22a92bf9b6a7ac12334e3cc
pavel242242 <pavel@chocholous.net>
2023-06-01 09:09:21 +0000

    ai token

--- chat.py
+++ chat.py
@@ -30,11 +30,11 @@ def findAnswer(query: str):
 
 question = st.sidebar.text_input("question")
 
-if os.getenv('openai_apitoken'):
-    st.sidebar.text("using openai token from env: "+ os.getenv('openai_apitoken')[0:6] + "...")
-    openai_token = os.getenv('openai_apitoken')
-else:
-    openai_token = st.sidebar.text_input("openai token")
+    if os.getenv('openai_apitoken'):
+        st.sidebar.text("using openai token from env: "+ os.getenv('openai_apitoken')[0:6] + "...")
+        openai_token = os.getenv('openai_apitoken')
+    else:
+        openai_token = st.sidebar.text_input("openai token")
 
 if st.sidebar.button("Click me") and question and openai_token:
     if OPENAI_KEY:
