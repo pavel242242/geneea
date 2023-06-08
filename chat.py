45f542bcd2b88554514e7cfc51084df8c146d6e0
pavel242242 <pavel@chocholous.net>
2023-06-08 12:33:29 +0000

    cleanup

--- chat.py
+++ chat.py
@@ -6,8 +6,9 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from pathlib import Path
 import os
-import pathlib
+
 import zipfile
+
 
 def loadExistingIndex(persist_directory: Path, embeddings):
     vectordb = Chroma(persist_directory=str(persist_directory),
@@ -19,9 +20,10 @@ def findAnswer(query: str):
     result = qa({"question": query, "chat_history": []})
     st.write(result["answer"])
     st.write("--")
-    st.write('sources:',
-             [d.metadata['source'] for d in result["source_documents"]])
+    st.write('sources:', [d.metadata['source']
+             for d in result["source_documents"]])
     st.write("--")
+
 
 def decompress_all_zips(directory_path, output_directory):
     for file in os.listdir(directory_path):
@@ -29,6 +31,8 @@ def decompress_all_zips(directory_path, output_directory):
         if file.endswith(".zip"):
             with zipfile.ZipFile(file_path, 'r') as zipf:
                 zipf.extractall(output_directory)
+
+
 PERSIST_PATH = "/data/in/files/chroma"
 if not os.path.exists('/data/in/files/chroma'):
     os.mkdir('/data/in/files/chroma')
@@ -36,11 +40,12 @@ if not os.path.exists('/data/in/files/chroma'):
     output_directory = '/data/in/files/chroma'
     decompress_all_zips(zip_file_to_decompress, output_directory)
 
-question = st.sidebar.text_input("question")
+question = st.sidebar.text_input("Ask your docs:")
 
 if os.getenv('openai_apitoken'):
     with st.sidebar:
-        st.write("using openai token from env: "+ os.getenv('openai_apitoken')[0:6] + "...")
+        st.write("using openai token from env: " +
+                 os.getenv('openai_apitoken')[0:6] + "...")
     openai_token = os.getenv('openai_apitoken')
 else:
     openai_token = st.sidebar.text_input("openai token")
